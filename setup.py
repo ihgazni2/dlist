1440658f1a596e05b6550698df6e96abf8746440
dli <terryinzaghi@163.com>
2018-12-04 18:21:19 +0000

    remove old build

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
       name="dlist",
-      version = "0.4",
+      version = "0.5",
       description="A list of dict , each dict has only one key.",
       author="dapeli",
       url="https://github.com/ihgazni2/dlist",
       author_email='terryinzaghi@163.com', 
       license="MIT",
-      long_description = "refer to .md files in https://github.com/ihgazni2/dlist",
+      long_description = "https://ihgazni2.github.io/dlist/"+"\n<br>"+long_description,
       classifiers=[
           'Environment :: Console',
           'Environment :: Web Environment',
